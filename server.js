5f00b3d4bc0902463e5314e1ef657b038622131d
Anthony Ramirez <tonyram@Anthonys-MacBook-Pro.local>
2017-09-14 21:27:56 -0400

    Start with a base of a RESTful API Setting up server for us to use

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require('express'),
+app = express(),
+port = process.env.PORT || 3000,
+mongoose = require('mongoose'),
+Sheet = require('./api/models/characterSheetModel'), //created model loading here
+bodyParser = require('body-parser');
+
+// mongoose instance connection url connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/CharacterSheetdb'); 
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+var routes = require('./api/routes/characterSheetRoutes'); //importing route
+routes(app); //register the route
+
+
+app.listen(port);
+
+
+console.log('Character Sheet RESTful API server started on: ' + port);
