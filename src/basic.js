445b2cb24fcfa31d7196cdac52bbb3ebbf60d062
Anthony Ramirez <tramirez1992@gmail.com>
2017-09-30 15:01:54 -0400

    Adding Stats Section

--- src/basic.js
+++ src/basic.js
@@ -8,6 +8,21 @@ var mysubclass = document.getElementById('selSubClass');
 var classValue = myclass.value;
 var subclassValue = mysubclass.value;
 
+var str = document.getElementById('Strength');
+var con = document.getElementById('Constitution');
+var dex = document.getElementById('Dexterity');
+var int = document.getElementById('Intelligence');
+var wis = document.getElementById('Wisdom');
+var cha = document.getElementById('Charisma');
+var strMod = document.getElementById('strMod').value;
+var conMod = 0;
+var dexMod = 0;
+var wisMod = 0;
+var intMod = 0;
+var chaMod = 0;
+
+var stats = document.getElementById('Stats');
+
 window.onload = function chooseSubRace(){
     var Dwarf = {
         hill: 'Hill Dwarf',
@@ -51,10 +66,7 @@ window.onload = function chooseSubRace(){
     var Tiefling = {
         tiefling: 'Tiefling'
     };
-
-
     race.addEventListener('change', choosing);
-
     function choosing() {
         raceValue = race.value;
         if (race.value === 'choose') {
@@ -70,7 +82,6 @@ window.onload = function chooseSubRace(){
         }
         console.log(document.getElementById('selSubRace').value);
     };
-
     function getRaces(raceValue) {
         
         if (raceValue === 'Dwarf') {
@@ -95,8 +106,8 @@ window.onload = function chooseSubRace(){
     }
 };
 
-addEventListener("change", chooseSubClass);
 
+addEventListener("change", chooseSubClass);
 function chooseSubClass(){
     var Barbarian = {
         berserker: 'Path of the Berserker',
@@ -209,6 +220,166 @@ function chooseSubClass(){
             return Wizard;
         }
     }
+};
+
+stats.addEventListener("input",getMod);
+function getMod() {
+    strValue = str.value;
+    conValue = con.value;
+    dexValue = dex.value;
+    wisValue = wis.value;
+    intValue = int.value;
+    chaValue = cha.value;
+
+    if (strValue <= 1){
+        strMod = -5;
+    } else if (strValue == 2 || strValue == 3){
+        strMod = -4;
+    } else if (strValue == 4 || strValue == 5){
+        strMod = -3;
+    } else if (strValue == 6 || strValue == 7){
+        strMod = -2;
+    } else if (strValue == 8 || strValue == 9){
+        strMod = -1;
+    } else if (strValue == 10 || strValue == 11){
+        strMod = 0;
+    } else if (strValue == 12 || strValue == 13){
+        strMod = 1;
+    } else if (strValue == 14 || strValue == 15){
+        strMod = 2;
+    } else if (strValue == 16 || strValue == 17){
+        strMod = 3;
+    } else if (strValue == 18 || strValue == 19){
+        strMod = 4;
+    } else if (strValue >= 20){
+        strMod = 5;
+    }
+
+    if (conValue <= 1){
+        conMod = -5;
+    } else if (conValue == 2 || conValue == 3){
+        conMod = -4;
+    } else if (conValue == 4 || conValue == 5){
+        conMod = -3;
+    } else if (conValue == 6 || conValue == 7){
+        conMod = -2;
+    } else if (conValue == 8 || conValue == 9){
+        conMod = -1;
+    } else if (conValue == 10 || conValue == 11){
+        conMod = 0;
+    } else if (conValue == 12 || conValue == 13){
+        conMod = 1;
+    } else if (conValue == 14 || conValue == 15){
+        conMod = 2;
+    } else if (conValue == 16 || conValue == 17){
+        conMod = 3;
+    } else if (conValue == 18 || conValue == 19){
+        conMod = 4;
+    } else if (conValue >= 20){
+        conMod = 5;
+    }    
+    if (dexValue <= 1){
+        dexMod = -5;
+    } else if (dexValue == 2 || dexValue == 3){
+        dexMod = -4;
+    } else if (dexValue == 4 || dexValue == 5){
+        dexMod = -3;
+    } else if (dexValue == 6 || dexValue == 7){
+        dexMod = -2;
+    } else if (dexValue == 8 || dexValue == 9){
+        dexMod = -1;
+    } else if (dexValue == 10 || dexValue == 11){
+        dexMod = 0;
+    } else if (dexValue == 12 || dexValue == 13){
+        dexMod = 1;
+    } else if (dexValue == 14 || dexValue == 15){
+        dexMod = 2;
+    } else if (dexValue == 16 || dexValue == 17){
+        dexMod = 3;
+    } else if (dexValue == 18 || dexValue == 19){
+        dexMod = 4;
+    } else if (dexValue >= 20){
+        dexMod = 5;
+    }
+
+    if (intValue <= 1){
+        intMod = -5;
+    } else if (intValue == 2 || intValue == 3){
+        intMod = -4;
+    } else if (intValue == 4 || intValue == 5){
+        intMod = -3;
+    } else if (intValue == 6 || intValue == 7){
+        intMod = -2;
+    } else if (intValue == 8 || intValue == 9){
+        intMod = -1;
+    } else if (intValue == 10 || intValue == 11){
+        intMod = 0;
+    } else if (intValue == 12 || intValue == 13){
+        intMod = 1;
+    } else if (intValue == 14 || intValue == 15){
+        intMod = 2;
+    } else if (intValue == 16 || intValue == 17){
+        intMod = 3;
+    } else if (intValue == 18 || intValue == 19){
+        intMod = 4;
+    } else if (intValue >= 20){
+        intMod = 5;
+    }
+
+    if (wisValue <= 1){
+        conMod = -5;
+    } else if (wisValue == 2 || wisValue == 3){
+        wisMod = -4;
+    } else if (wisValue == 4 || wisValue == 5){
+        wisMod = -3;
+    } else if (wisValue == 6 || wisValue == 7){
+        wisMod = -2;
+    } else if (wisValue == 8 || wisValue == 9){
+        wisMod = -1;
+    } else if (wisValue == 10 || wisValue == 11){
+        wisMod = 0;
+    } else if (wisValue == 12 || wisValue == 13){
+        wisMod = 1;
+    } else if (wisValue == 14 || wisValue == 15){
+        wisMod = 2;
+    } else if (wisValue == 16 || wisValue == 17){
+        wisMod = 3;
+    } else if (wisValue == 18 || wisValue == 19){
+        wisMod = 4;
+    } else if (wisValue >= 20){
+        wisMod = 5;
+    }    
+
+    if (chaValue <= 1){
+        chaMod = -5;
+    } else if (chaValue == 2 || chaValue == 3){
+        chaMod = -4;
+    } else if (chaValue == 4 || chaValue == 5){
+        chaMod = -3;
+    } else if (chaValue == 6 || chaValue == 7){
+        chaMod = -2;
+    } else if (chaValue == 8 || chaValue == 9){
+        chaMod = -1;
+    } else if (chaValue == 10 || chaValue == 11){
+        chaMod = 0;
+    } else if (chaValue == 12 || chaValue == 13){
+        chaMod = 1;
+    } else if (chaValue == 14 || chaValue == 15){
+        chaMod = 2;
+    } else if (chaValue == 16 || chaValue == 17){
+        chaMod = 3;
+    } else if (chaValue == 18 || chaValue == 19){
+        chaMod = 4;
+    } else if (chaValue >= 20){
+        chaMod = 5;
+    }    
+
+    document.getElementById("strMod").innerHTML = strMod;
+    document.getElementById("conMod").innerHTML = conMod;
+    document.getElementById("dexMod").innerHTML = dexMod;
+    document.getElementById("intMod").innerHTML = intMod; 
+    document.getElementById("wisMod").innerHTML = wisMod;
+    document.getElementById("chaMod").innerHTML = chaMod;   
 };
 
 
